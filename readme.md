71de6b0263a4b2cee7c90896dba701dc573d453b
tomo <tomo.mouse.9086@gmail.com>
2018-08-04 03:54:04 +0900

    できた

--- readme.md
+++ readme.md
@@ -1,1 +1,18 @@
 # vue-markdown
+## Usage
+
+#### 1. install packages
+install "markdown-it", "lodash.debounce", and "markdown-it-katex"
+
+```shell
+$ npm install --save markdown-it lodash.debounce markdown-it-katex
+```
+
+#### 2. add KaTeX CSS to your index.html
+add the tag below to the last line of head tag
+
+```html
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css">
+```
+
+#### 3. copy MdEditor.vue into your project
--- src/vue/components/MdEditor.vue
+++ src/vue/components/MdEditor.vue
@@ -7,17 +7,17 @@
 
 <script>
 	const MarkdownIt = require("markdown-it")
-	const katex = require("katex")
 	const debounce = require("lodash.debounce")
 
 	const md = new MarkdownIt()
+	md.use(require("markdown-it-katex"), {"throwOnError" : false, "errorColor" : "#cc0000"})
 
 	module.exports = {
 		components: {
 		},
 		data() {
 			return {
-				input: "# hello"
+				input: "# hello\n$S = \\frac{Q_{rev}}{T}$"
 			}
 		},
 		methods: {
@@ -27,6 +27,7 @@
 		},
 		computed: {
 			compiledMarkdown() {
+				// console.log(this.input)
 				return md.render(this.input)
 			}
 		}
@@ -35,33 +36,33 @@
 
 <style scoped>
 	.editor-container {
-	  margin: 0;
-	  height: 100%;
-	  font-family: "Helvetica Neue", Arial, sans-serif;
-	  color: #333;
+		margin: 0;
+		height: 100%;
+		/*font-family: "Helvetica Neue", Arial, sans-serif;*/
+		color: #333;
 	}
 
 	textarea, .editor-container div {
-	  display: inline-block;
-	  width: 49%;
-	  height: 100%;
-	  vertical-align: top;
-	  box-sizing: border-box;
-	  padding: 0 20px;
+		display: inline-block;
+		width: 49%;
+		height: 100%;
+		vertical-align: top;
+		box-sizing: border-box;
+		padding: 0 20px;
 	}
 
 	textarea {
-	  border: none;
-	  border-right: 1px solid #ccc;
-	  resize: none;
-	  outline: none;
-	  background-color: #f6f6f6;
-	  font-size: 14px;
-	  font-family: "Monaco", courier, monospace;
-	  padding: 20px;
+		border: none;
+		border-right: 1px solid #ccc;
+		resize: none;
+		outline: none;
+		background-color: #f6f6f6;
+		font-size: 14px;
+		/*font-family: "Monaco", courier, monospace;*/
+		padding: 20px;
 	}
 
 	code {
-	  color: #f66;
+		color: #f66;
 	}
 </style>
